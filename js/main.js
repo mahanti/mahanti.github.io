64f5c9bfc283034f2409a10f546ed9215e07efcc
Arjun Mahanti <arjun.mahanti@gmail.com>
2015-05-19 17:11:35 -0700

    new photos and more thorough sizzlin stuff

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
-$(document).ready(function() {
-  $( ".post_link" ).click(function() {
-    $( "#body_content" ).fadeOut( "slow", function() {
+/*$(document).ready(function(){
+    // to fade in on page load
+    $("#body_content").css("display", "none");
+    $("#body_content").fadeIn(200); 
+    // to fade out before redirect
+    $('a').click(function(e){
+        redirect = $(this).attr('href');
+        e.preventDefault();
+        $('#body_content').fadeOut(200, function(){
+            document.location.href = redirect
+        });
     });
-  });
-});
-
+})*/
