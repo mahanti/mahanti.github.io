26028a0021d6acd48e6b1841c067eef08f75fff9
Arjun Mahanti <arjun.mahanti@gmail.com>
2015-09-22 15:13:01 -0700

    new stuff

--- lab/squares/square.js
+++ lab/squares/square.js
@@ -4,18 +4,19 @@ var window_width;
 var paper;
 var tilesholder;
 
+//var colors = new Array("#1eb0b1", "#f3dbc7", "#f25526", "#ffc885", "#364d7f", "#862b32" , "#1c8f9e")
 var colors = new Array("#4312AE","#472B83","#280671","#7446D7","#8E6DD7","#AA00A2","#7F207B","#6E0069","#D435CD","#D460CF","#1049A9","#29497F","#052C6E","#4479D4","#6A92D4","#FF9A00","#FF3500","#FF6840");
 
 function generateTiles() {
-        var rect_height = 25;
-        var rect_width = 25;
+        var rect_height = 10;
+        var rect_width = 10;
 
         //x, y, width, height
         var clr = 0;
         var idx = 0;
         var tile_count = 0;
-        for(var i=0;i<window.window_width;i+=25){
-            for (var j=0;j<window.window_height;j+=25) {
+        for(var i=0;i<window.window_width;i+=12){
+            for (var j=0;j<window.window_height;j+=12) {
                 clr = Math.floor((Math.random()*17)+1);
                 var tile = paper.rect(i,j,rect_width, rect_height);
                 tile.attr("fill", colors[clr]);
@@ -23,6 +24,7 @@ function generateTiles() {
                 tile.mouseover(function(){
                     var new_colr = Math.floor((Math.random()*17)+1);
                     this.stop().animate({fill: colors[new_colr] }, 100);
+                    console.log(this)
                 });
                 tilesholder.push(tile);
                 tile_count++;
