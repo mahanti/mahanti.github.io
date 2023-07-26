800c834a98c585e1ece5a36a93f249990bc31aae
Arjun <arjun@angellist.com>
2023-07-26 15:39:52 -0400

    callouts

--- js/script.js
+++ js/script.js
@@ -22,3 +22,33 @@ navLink.forEach(function (link) {
     navList.classList.remove('navlist-active');
   });
 });
+
+/*
+const imageLinks = document.querySelectorAll(".image-link");
+  const imageContainer = document.querySelector(".image-container");
+  const defaultContent = document.querySelector(".default-content");
+
+  imageLinks.forEach((link) => {
+    const image = link.querySelector(".hover-image");
+
+    link.addEventListener("mouseover", () => {
+      defaultContent.style.opacity = "0";
+      imageContainer.appendChild(image);
+      image.style.display = "block";
+
+      // Delay the opacity change to ensure the image is in the document flow
+      setTimeout(() => {
+        image.style.opacity = "1";
+      }, 0);
+    });
+
+    link.addEventListener("mouseout", () => {
+      image.style.opacity = "0";
+      // Delay the display change to allow the transition to complete
+      setTimeout(() => {
+        image.style.display = "none";
+        defaultContent.style.opacity = "1";
+      }, 500); // Match this to the length of your transition
+    });
+  });
+*/
