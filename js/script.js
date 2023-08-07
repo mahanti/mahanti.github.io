4f6fb0346a3e195eccbf73ba095275612135dbdc
Arjun <arjun@angellist.com>
2023-08-07 17:08:17 -0400

    new AL

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const navBtn = document.querySelector('.navlist-button');
 const navList = document.querySelector('.navlist');
 const navLink = document.querySelectorAll('.navlist-link');
+const row = document.querySelectorAll('.row');
 
 //Initiating classes for open/close
 navBtn.addEventListener('click', (e) => {  
@@ -22,6 +23,23 @@ navLink.forEach(function (link) {
     navList.classList.remove('navlist-active');
   });
 });
+
+const currentHour = new Date().getHours();
+let links = document.getElementsByTagName("a");
+if (currentHour >= 7 && currentHour < 18) {
+    document.body.className = "day";
+    for(let i=0; i < links.length; i++) {
+        links[i].classList.remove("night");
+        links[i].classList.add("day");
+    }
+} else {
+    document.body.className = "night";
+    for(let i=0; i < links.length; i++) {
+        links[i].classList.remove("day");
+        links[i].classList.add("night");
+    }
+}
+
 
 /*
 const imageLinks = document.querySelectorAll(".image-link");
